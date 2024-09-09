1480f88a8d0a03bd46000ec403811e0954df6749
Chrisreynolds0508 <Chris.reynolds0508@gmail.com>
2024-09-09 19:59:10 +1000

    updated homepage handlebars page

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -16,24 +16,24 @@ router.post('/', async (req, res) => {
   }
 });
 
-// router.delete('/:id', withAuth, async (req, res) => {
-//   try {
-//     const projectData = await Project.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const projectData = await Project.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
 
-//     if (!projectData) {
-//       res.status(404).json({ message: 'No review found with this id!' });
-//       return;
-//     }
+    if (!projectData) {
+      res.status(404).json({ message: 'No review found with this id!' });
+      return;
+    }
 
-//     res.status(200).json(projectData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
