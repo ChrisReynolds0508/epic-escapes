3e918c904c7e76d15706a7b21c01972330a6d9c8
Chrisreynolds0508 <Chris.reynolds0508@gmail.com>
2024-09-10 19:53:46 +1000

    added get requests to reviewRoutes

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -2,6 +2,33 @@ const router = require('express').Router();
 const { Reviews } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', async (req, res) => {
+  try {
+    const reviewData = await Reviews.findAll();
+
+    res.status(200).json(reviewData);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json(err);
+  }
+}
+);
+
+router.get('/:id', async (req, res) => {
+  try {
+    const reviewData = await Reviews.findByPk(req.params.id);
+
+    if (!reviewData) {
+      res.status(404).json({ message: 'No review found with this id!' });
+      return;
+    }
+
+    res.status(200).json(reviewData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+);
 router.post('/', async (req, res) => {
   try {
     const newReview = await Reviews.create({
